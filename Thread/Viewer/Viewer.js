776ec8016321a919f8acbaa45074087ecd0150e5
Genbu Hase <hasegenbu@gmail.com>
2017-10-19 19:20:05 +0900

    OK

--- Thread/Viewer/Viewer.js
+++ Thread/Viewer/Viewer.js
@@ -20,6 +20,8 @@ window.addEventListener("DOMContentLoaded", () => {
 		doc.querySelector("#Header_Title").textContent = `${res.title}`;
 
 		if (res.password) {
+			doc.querySelector("IFrame.mdl-layout__content").src = "Thread/Viewer/Auth/";
+			
 			doc.querySelector("#Dialogs_Thread_PasswordConfirmer_Password").value = res.password;
 			doc.querySelector("#Dialogs_Thread_PasswordConfirmer").showModal();
 		}
--- assets/libraries/includes/Core.js
+++ assets/libraries/includes/Core.js
@@ -14,6 +14,12 @@ try {
 window.addEventListener("DOMContentLoaded", () => {
 	locales.apply(this);
 
+	DOM("@Main").forEach(elem => {
+		["mdl-cell", "mdl-cell--2-offset", "mdl-cell--8-col", "mdl-shadow--4dp", "mdl-color--white", "mdl-color-text--grey-800"].forEach(className => {
+			elem.classList.add(className);
+		});
+	});
+	
 	DOM('@A[Href]:Not([Target]):Not([Href^="javascript:"])').forEach((elem) => {
 		elem.addEventListener("click", (event) => {
 			event.preventDefault();
