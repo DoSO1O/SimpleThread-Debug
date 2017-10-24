d772f62273b15008d24f5280b103a2c61d42d4f4
Genbu Hase <hasegenbu@gmail.com>
2017-10-24 15:01:01 +0900

    OK

--- dev/rest/main.js
+++ dev/rest/main.js
@@ -1,11 +1,15 @@
+let csl = null;
+
 window.addEventListener("DOMContentLoaded", () => {
-	document.body.appendChild(new Text(Base));
+	csl = new JSCAFD();
+	csl.log(Base);
+	csl.log(Base.send);
 
 	DOM("#Base_send").addEventListener("click", () => {
 		try {
 			Base.send(DOM("#Base_send-Arg1").value);
 		} catch (err) {
-			document.body.appendChild(new Text(err));
+			csl.log(err);
 		}
 	});
 });
