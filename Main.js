866938a45268721f9754ff827d0bd5c841190b4e
Genbu Hase <hasegenbu@gmail.com>
2017-10-31 18:19:04 +0900

    Firebaseの階層変更に伴う更新

--- Main.js
+++ Main.js
@@ -92,7 +92,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	DOM("#Header_SignInOut").addEventListener("click", () => {
 		switch (DOM("#Header_SignInOut").dataset.locales) {
 			case "main.signIn":
-				base.signIn(["https://www.googleapis.com/auth/plus.login"]);
+				base.signInWithGoogle(["https://www.googleapis.com/auth/plus.login"]);
 				break;
 				
 			case "main.signOut":
